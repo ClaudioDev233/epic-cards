44f1995cd1acc5844367a8bdee5c00311f4ebec1
Claudio <claudio.junior233@gmail.com>
2022-01-11 21:25:50 -0300

    css changes

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
 export default function Menu(){
-    return <nav>
-        <h1>Nome Loja</h1>
-        <ul>
-            <li>Home</li>
-            <li>Carrinho</li>
-            <li>Menu</li>
+    return <nav className="menu">
+        <h1 className="menu-title">Card Shop</h1>
+        <ul className="menu-list">
+            <li><Link className="menu-list-item" to="/">Home</Link></li>
+            <li><Link className="menu-list-item" to="/cart">Carrinho</Link></li>
+            <li><Link className="menu-list-item" to="/about">About</Link></li>
         </ul>
     </nav>
 }
