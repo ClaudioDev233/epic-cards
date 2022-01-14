72f1d97fa3690bcc6140acad18d3c7c20b61d7ef
Claudio <claudio.junior233@gmail.com>
2022-01-13 21:48:58 -0300

    added dependencies

--- src/components/contexts/cart/index.js
+++ src/components/contexts/cart/index.js
@@ -1,15 +1,24 @@
 import { createContext,useState,useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export const CartContext = createContext([]);
 
 
 export function CartProvider({children}){
     
+    
+
     const [cart, setCart] = useState([])
 
     function handleAddCart(item){
-        setCart([...cart, item])
-        localStorage.setItem("cart", JSON.stringify([...cart, item]))
+
+        const newCard =  {
+            ... item ,
+            idCard : uuidv4()
+        }
+
+        setCart([...cart, newCard])
+        localStorage.setItem("cart", JSON.stringify([...cart, newCard]))
     }
     
     useEffect(()=>{
@@ -20,12 +29,27 @@ export function CartProvider({children}){
         handleLocalStorage()
     },[])
 
+   function handleRemoveCard(id){
+    const cartFiltered = cart.filter( item => { 
+        if(item.idCard === id) {
+            return false // remove do carrinho
+        } else {
+            return true // continua no carrinho
+        }
+    })
+        setCart(cartFiltered)
+    localStorage.setItem("cart", JSON.stringify(cartFiltered))
+
+    }
+
+
     return (
         <CartContext.Provider 
         value={{
             cart: cart,
-            addItem: handleAddCart
-        }}
+            addItem: handleAddCart,
+            remove: handleRemoveCard
+            }}
         >
             {children}
         </CartContext.Provider>
--- src/components/pages/cart/index.jsx
+++ src/components/pages/cart/index.jsx
@@ -1,16 +1,46 @@
 import { useContext } from 'react'
 import {CartContext }from '../../contexts/cart'
+import { MdDeleteOutline} from "react-icons/md";
 
 export default function Cart(){
-   const {cart} = useContext(CartContext)
+   const {cart , remove} = useContext(CartContext)
 
-    return (
-        <div>
-        {cart.map(item => <div>{item.name} - {
+   return (
+    <div>
+      <table className='table-cart'>
+        <thead>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Deletar</th>
+        </thead>
+        <tbody>
+          {cart.map(item =>
+            <tr>
+              <td>
+                <img
+                  src={item.card_images[0].image_url_small}
+                  alt="Foto da carta"
+                  className="table-image"
+                />
+              </td>
+              <td>{item.name}</td>
+              <td>{
                 new Intl.NumberFormat(
                   'pt-BR',
                   { style: 'currency', currency: 'BRL' }
-                ).format(item.card_prices[0].cardmarket_price)}</div>)}
-       </div>
-    ) 
-} 
+                ).format(item.card_prices[0].cardmarket_price)
+              }</td>
+              <td><MdDeleteOutline size={30} onClick={() => remove(item.idCard)}/></td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+
+    </div>
+  );
+}
+
+
+  
